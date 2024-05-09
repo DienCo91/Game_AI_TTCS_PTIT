8e4197d48953d67b434a61623ece684b2f6d3a93
SIU <90703662+NguyenNhatMinh-BYS@users.noreply.github.com>
2024-05-10 00:46:46 +0700

    full source code

---
+++ Chess/Assets/Scripts/Chuyenscence.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Chuyenscence : MonoBehaviour
+{
+    public void choivoinguoi(){
+        SceneManager.LoadScene("Game");
+    }
+    public void choivoimay(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void dauhang(){
+        SceneManager.LoadScene("MapScenes");
+    }
+    public void thoat(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void huongdan(){
+        SceneManager.LoadScene("Huongdan");
+    }
+    
+}
